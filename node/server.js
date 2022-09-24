78f5f57fa26b13b7c8e128d611b2268967d5bd2a
AlexMeow <oo587868@hotmail.com>
2022-09-25 02:04:10 +0800

    Update server.js

--- node/server.js
+++ node/server.js
@@ -19,25 +19,22 @@ server.listen(PORT, '0.0.0.0', () => {
 const wss = new SocketServer({ server })
 //當有 client 連線成功時
 wss.on('connection', ws => {
-//  ws.send("This website is for test purpose, please do not leave any personal information here.");
+  ws.send("This website is for test purpose, please do not leave any personal information here.");
   console.log('Client connected');
   // 當收到client消息時
-//  ws.on('message', data => {
+  ws.on('message', data => {
     // 收回來是 Buffer 格式、需轉成字串
-//    data = data.toString()
-//    console.log(data) // 可在 terminal 看收到的訊息
-
-    /// 發送消息給client 
-//    ws.send(data)
+    data = data.toString();
+    console.log(data) // 可在 terminal 看收到的訊息
 
     /// 發送給所有client： 
-//    let clients = wss.clients  //取得所有連接中的 client
-//    clients.forEach(client => {
-//        client.send(data)  // 發送至每個 client
-//    })
-//  })
+    let clients = wss.clients;  //取得所有連接中的 client
+    clients.forEach(client => {
+        client.send(data)  // 發送至每個 client
+    })
+  });
   // 當連線關閉
   ws.on('close', () => {
     console.log('Close connected');
-  })
+  });
 })
